5810eaa48dcee59738058dfe125ff18e0a437366
Poltergeistzx <102504102+Poltergeistzx@users.noreply.github.com>
2023-04-08 17:10:32 +0300

    formatting

--- media/lua/client/Ladders/Ladders.lua
+++ media/lua/client/Ladders/Ladders.lua
@@ -1,37 +1,37 @@
-local Ladders = {}
-
 --[[
 	Set sprite properties for climbing, square takes properties from objects, objects from sprites.
 	To prevent falling during climbing we make the custom sprites more persistent and able to pass their properties to the square.
 	IDs used are in the range for fileNumber 100, used by mod SpearTraps
 --]]
 
+local Ladders = {}
+
 Ladders.idW, Ladders.idN = 26476542, 26476543
 Ladders.climbSheetTopW = "TopOfLadderW"
 Ladders.climbSheetTopN = "TopOfLadderN"
 
---used by joypad
-function Ladders.getLadderObject(square)
-	local objects = square:getObjects()
-	for i = 0, objects:size() - 1 do
-		local object = objects:get(i)
-		local sprite = object:getSprite()
-		if sprite then
-			local prop = sprite:getProperties()
-			local name = sprite:getName()
-			if prop:Is(IsoFlagType.climbSheetN) or prop:Is(IsoFlagType.climbSheetS) or prop:Is(IsoFlagType.climbSheetE) or prop:Is(IsoFlagType.climbSheetW) then
-            	--if Ladders.player then
-				--	if Ladders.ladderTiles[name] then
-				--		Ladders.player:setVariable("ClimbLadder", true)
-				--	else
-				--		Ladders.player:clearVariable("ClimbLadder")
-				--	end
-				--end
-				return object
-			end
-		end
-	end
-end
+--was used by joypad
+--function Ladders.getLadderObject(square)
+--	local objects = square:getObjects()
+--	for i = 0, objects:size() - 1 do
+--		local object = objects:get(i)
+--		local sprite = object:getSprite()
+--		if sprite then
+--			local prop = sprite:getProperties()
+--			local name = sprite:getName()
+--			if prop:Is(IsoFlagType.climbSheetN) or prop:Is(IsoFlagType.climbSheetS) or prop:Is(IsoFlagType.climbSheetE) or prop:Is(IsoFlagType.climbSheetW) then
+--            	if Ladders.player then
+--					if Ladders.ladderTiles[name] then
+--						Ladders.player:setVariable("ClimbLadder", true)
+--					else
+--						Ladders.player:clearVariable("ClimbLadder")
+--					end
+--				end
+--				return object
+--			end
+--		end
+--	end
+--end
 
 function Ladders.getTopOfLadder(square, north)
 	local objects = square:getObjects()
@@ -92,7 +92,6 @@ function Ladders.removeTopOfLadder(square)
 end
 
 --climbSheetTop_ check: stops for poles at proper square
---if can climb and climbTop then remove topOfLadder so player can climb higher
 function Ladders.makeLadderClimbable(square, north)
 
 	local x, y, z = square:getX(), square:getY(), square:getZ()
@@ -142,14 +141,14 @@ end
 -- because then it respects their vanilla rebindings.
 function Ladders.OnKeyPressed(key)
 	if key == getCore():getKey("Interact") then
-        local player = getPlayer()
+		local player = getPlayer()
 		if not player or player:isDead() then return end
 		if MainScreen.instance:isVisible() then return end
 
-        -- Will store last player to attempt to climb a ladder.
-        Ladders.player = player
+		-- Will store last player to attempt to climb a ladder.
+		Ladders.player = player
 
-        local square = player:getSquare()
+		local square = player:getSquare()
 		Ladders.makeLadderClimbableFromTop(square)
 		Ladders.makeLadderClimbableFromBottom(square)
 	end
@@ -219,37 +218,37 @@ function Ladders.chooseAnimVar(square,topObject)
 	end
 end
 
-Ladders.westLadderTiles = { 
+Ladders.westLadderTiles = {
 	"industry_02_86", "location_sewer_01_32", "industry_railroad_05_20", "industry_railroad_05_36", "walls_commercial_03_0",
-	"edit_ddd_RUS_decor_house_01_16", "edit_ddd_RUS_decor_house_01_19", "edit_ddd_RUS_industry_crane_01_72", 
-	"edit_ddd_RUS_industry_crane_01_73", "rus_industry_crane_ddd_01_24", "rus_industry_crane_ddd_01_25", 
+	"edit_ddd_RUS_decor_house_01_16", "edit_ddd_RUS_decor_house_01_19", "edit_ddd_RUS_industry_crane_01_72",
+	"edit_ddd_RUS_industry_crane_01_73", "rus_industry_crane_ddd_01_24", "rus_industry_crane_ddd_01_25",
 	"A1 Wall_48", "A1 Wall_80", "A1_CULT_36", "aaa_RC_6", "trelai_tiles_01_30", "trelai_tiles_01_38",
 	"industry_crane_rus_72", "industry_crane_rus_73"
 }
 
-Ladders.northLadderTiles = { 
+Ladders.northLadderTiles = {
 	"location_sewer_01_33", "industry_railroad_05_21", "industry_railroad_05_37",
-	"edit_ddd_RUS_decor_house_01_17", "edit_ddd_RUS_decor_house_01_18", 
-	"edit_ddd_RUS_industry_crane_01_76", "edit_ddd_RUS_industry_crane_01_77", 
+	"edit_ddd_RUS_decor_house_01_17", "edit_ddd_RUS_decor_house_01_18",
+	"edit_ddd_RUS_industry_crane_01_76", "edit_ddd_RUS_industry_crane_01_77",
 	"A1 Wall_49", "A1 Wall_81", "A1_CULT_37", "aaa_RC_14", "trelai_tiles_01_31",
 	"trelai_tiles_01_39", "industry_crane_rus_76", "industry_crane_rus_77",
 }
 
 for index = 1, 62 do
 	local name = "basement_objects_02_" .. index
-    if index % 2 == 0 then
-        Ladders.westLadderTiles[#Ladders.westLadderTiles + 1] = name
-    else
-        Ladders.northLadderTiles[#Ladders.northLadderTiles + 1] = name
-    end
+	if index % 2 == 0 then
+		Ladders.westLadderTiles[#Ladders.westLadderTiles + 1] = name
+	else
+		Ladders.northLadderTiles[#Ladders.northLadderTiles + 1] = name
+	end
 end
 
-Ladders.holeTiles = { 
-	"floors_interior_carpet_01_24" 
+Ladders.holeTiles = {
+	"floors_interior_carpet_01_24"
 }
 
-Ladders.poleTiles = { 
-	"recreational_sports_01_32", "recreational_sports_01_33" 
+Ladders.poleTiles = {
+	"recreational_sports_01_32", "recreational_sports_01_33"
 }
 
 --- Generate Table for faster check during anim choice
