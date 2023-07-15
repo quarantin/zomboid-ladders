2eb067ce2c66b18d668ceed2f8550f441272e973
Poltergeistzx <igor.mov0@gmail.com>
2023-07-15 17:09:17 +0300

    Complete Rewrite
    - Add animation for ladder climbing
    - List more sprites for climbing
    - Improve performance and stability
    - No more falling from ladders while climbing
    
    Use OnLoadedTileDefinitions event instead of Loadgrisquare
    Fix TopOfLadder objects to have persistent properties
    Check square properties directly, without looping through all objects where possible
    Adjust the structure of ladder tiles tables, add a lot more ladder tiles
    Gamepad will now use it's own table
    Changed hook from getBestBButtonAction to testBButtonAction
    Limit Ladder check to once per square, refresh when object is added.
    Add climb prompt for all climbable objects.
    Fixed a small bug in gamepad trigger when climbing down.
    Add new animation for climbing ladders, poles and other climbables excluded.
    
    Co-authored-by: Zendachi <zendachikazenai@gmail.com>

--- media/lua/client/Ladders/Ladders_GamePad.lua
+++ media/lua/client/Ladders/Ladders_GamePad.lua
@@ -2,7 +2,10 @@
 
 local Ladders = require('Ladders/Ladders')
 
-Ladders.validGamepadInput = function(playerIndex, button)
+local GamePad = {}
+GamePad.testedSq = {}
+
+GamePad.validGamepadInput = function(playerIndex, button)
 	playerIndex = (playerIndex or 0)
 	local player = getSpecificPlayer(playerIndex)
 	if not (player and player:isAlive()) then return end
@@ -10,12 +13,16 @@ Ladders.validGamepadInput = function(playerIndex, button)
 	return player
 end
 
-Ladders.triggerGamepadClimbing = function(buttonPromptData, button, square, down)
+GamePad.triggerGamepadClimbing = function(buttonPromptData, button, square, down)
 	local playerIndex = buttonPromptData.player
-	local player = Ladders.validGamepadInput(playerIndex, button)
+	local player = GamePad.validGamepadInput(playerIndex, button)
 	local location = player:getSquare()
-
+	
 	if (MainScreen.instance and MainScreen.instance:isVisible()) or not (player and location) then return end
+
+	-- Used for setting animatio state.
+    -- Will store last player to attempt to climb a ladder.
+    --Ladders.player = player
 
 	if down then -- Am I serious? Unfortunately . . . Yes.
 
@@ -33,24 +40,36 @@ Ladders.triggerGamepadClimbing = function(buttonPromptData, button, square, down
 				player:climbThroughWindow(window, 4)
 			elseif thumpable and thumpable:canClimbThrough(player) then
 				player:climbThroughWindow(thumpable, 4)
+			else
+				player:climbDownSheetRope()
 			end
-
-			player:climbDownSheetRope()
 
 			return
 		end
-
+		
 		local hoppable = location:getHoppableThumpableTo(square)
 		local wall = location:getWallHoppableTo(square)
 
 		if hoppable or wall then
 			local direction = player:getDir()
-			if IsoWindow.canClimbThroughHelper(player, location, square, direction == IsoDirections.N or direction == IsoDirections.S) then
-				player:climbOverFence(player:getDir())
+			-- Correction to prevent errors from being thrown when player isn't facing a ladder well enough.
+			if direction == IsoDirections.NE then
+				direction = IsoDirections.E
+			elseif direction == IsoDirections.SW then
+				direction = IsoDirections.S
+			elseif direction == IsoDirections.SE then
+				local option = location:getAdjacentSquare(IsoDirections.S)
+				if option and IsoWindow.isTopOfSheetRopeHere(option) and player:canClimbDownSheetRope(option) then
+					direction = IsoDirections.S
+				else -- There must be a ladder here, or the prompt wouldn't be visible in the first place.
+					direction = IsoDirections.E
+				end
 			end
-
-			player:climbDownSheetRope()
-
+			if IsoWindow.canClimbThroughHelper(player, location, square, direction == IsoDirections.N or direction == IsoDirections.S) then
+				player:climbOverFence(direction)
+			else
+				player:climbDownSheetRope()
+			end
 			return
 		end
 
@@ -59,10 +78,9 @@ Ladders.triggerGamepadClimbing = function(buttonPromptData, button, square, down
 		if frame then
 			if (IsoWindowFrame.canClimbThrough(frame, player)) then
 				player:climbThroughWindowFrame(frame)
+			else
+				player:climbDownSheetRope()
 			end
-
-			player:climbDownSheetRope()
-
 			return
 		end
 
@@ -72,94 +90,69 @@ Ladders.triggerGamepadClimbing = function(buttonPromptData, button, square, down
 		player:setZ(square:getZ())
 		player:climbDownSheetRope()
 	else -- Everything is immeasurably easier.
-		Ladders.enRoute = true
+		GamePad.enRoute = true
 		-- Walks to AND climbs the rope, rather than sort of teleporting to it
 		-- (which is what happens if you use player:climbSheetRope() below).
 		ISWorldObjectContextMenu.onClimbSheetRope(nil, square, false, playerIndex)
 	end
 end
 
-Ladders.patchBestBButtonAction = function()
+GamePad.patchBestBButtonAction = function()
 
-	-- Safe back-up in your module for others who may need this!
-	Ladders.ISButtonPrompt = Ladders.ISButtonPrompt or {
-		getBestBButtonAction = ISButtonPrompt.getBestBButtonAction
-	}
+	GamePad["ISButtonPrompt.testBButtonAction"] = GamePad["ISButtonPrompt.testBButtonAction"] or ISButtonPrompt.testBButtonAction
+	ISButtonPrompt.testBButtonAction = function(self,dir)
+		if not self.bPrompt then
 
-	function ISButtonPrompt:getBestBButtonAction(direction)
+			local player = getSpecificPlayer(self.player)
+			local square = player:getSquare()
 
-		-- Calling getBestBButtonAction back-up in the module created above.
-		Ladders.ISButtonPrompt.getBestBButtonAction(self, original)
+			if GamePad.testedSq[self] ~= square then
+				GamePad.testedSq[self] = square
+				Ladders.player = player
+				Ladders.makeLadderClimbableFromTop(square)
+				Ladders.makeLadderClimbableFromBottom(square)
+			end
 
-		if self.bPrompt and self.bPrompt ~= getText("ContextMenu_Climb_through") and self.bPrompt ~= getText("ContextMenu_Climb_over") then return end
-
-		local playerIndex = self.player
-		local player = getSpecificPlayer(playerIndex)
-		local square = player:getSquare()
-
-		-- This will prevent exceptions when you teleport and either
-		--  your target location or its objects have not yet spawned.
-		local ladder = (square and square:getObjects() and Ladders.getLadderObject(square))
-
-		if ladder then
-			Ladders.makeLadderClimbableFromTop(square)
-			Ladders.makeLadderClimbableFromBottom(square)
-			self:setBPrompt(getText("UI_Ladders_Climb"), Ladders.triggerGamepadClimbing, Joypad.BButton, square, false)
-			return
-		end
-
-		local original = direction
-
-		direction = direction or player:getDir()
-
-		if square then
-			if direction == IsoDirections.NE then
-				square = square:getAdjacentSquare(IsoDirections.N) or square:getAdjacentSquare(IsoDirections.E)
-			elseif direction == IsoDirections.NW then
-				square = square:getAdjacentSquare(IsoDirections.N) or square:getAdjacentSquare(IsoDirections.W)
-			elseif direction == IsoDirections.SE then
-				square = square:getAdjacentSquare(IsoDirections.S) or square:getAdjacentSquare(IsoDirections.E)
-			elseif direction == IsoDirections.SW then
-				square = square:getAdjacentSquare(IsoDirections.S) or square:getAdjacentSquare(IsoDirections.W)
-			else -- Direction is N, S, E, or W
-				square = square:getAdjacentSquare(direction)
+			local hasClimbFlag
+			if dir == IsoDirections.W and square:getProperties():Is(IsoFlagType.climbSheetW)
+			or dir == IsoDirections.N and square:getProperties():Is(IsoFlagType.climbSheetN)
+			or dir == IsoDirections.E and square:getProperties():Is(IsoFlagType.climbSheetE)
+			or dir == IsoDirections.S and square:getProperties():Is(IsoFlagType.climbSheetS)
+			then hasClimbFlag = true
+			end
+			if hasClimbFlag and player:canClimbSheetRope(square) then
+				self:setBPrompt(getText("UI_Ladders_Climb"), GamePad.triggerGamepadClimbing, Joypad.BButton, square, false)
+			else
+				square = square:getAdjacentSquare(dir)
+				if square and IsoWindow.isTopOfSheetRopeHere(square) and player:canClimbDownSheetRope(square) then
+					self:setBPrompt(getText("UI_Ladders_Climb"), GamePad.triggerGamepadClimbing, Joypad.BButton, square, true)
+				end
 			end
 		end
 
-		below = square and getSquare(math.floor(square:getX()), math.floor(square:getY()), math.floor(square:getZ() - 1))
-
-		-- ladder = (square and square:getObjects() and Ladders.getLadderObject(square))
-
-		ladder = (square and player:canClimbDownSheetRope(square))
-
-		ladderBelow = (below and below:getObjects() and Ladders.getLadderObject(below))
-
-		if ladder then
-			self:setBPrompt(getText("UI_Ladders_Climb"), Ladders.triggerGamepadClimbing, Joypad.BButton, square, true)
-			return
-		elseif ladderBelow then -- Controller testing suggests need to do this from the bottom of a ladder to work properly.
-			Ladders.makeLadderClimbableFromTop(below)
-			Ladders.makeLadderClimbableFromBottom(below)
-			self:setBPrompt(getText("UI_Ladders_Climb"), Ladders.triggerGamepadClimbing, Joypad.BButton, square, true)
-			return
-		end
-
+		return GamePad["ISButtonPrompt.testBButtonAction"](self,dir)
 	end
-
 end
 
-Events.OnGameStart.Add(Ladders.patchBestBButtonAction)
+function GamePad.OnObjectAdded()
+	table.wipe(GamePad.testedSq)
+end
+
+Events.OnGameStart.Add(GamePad.patchBestBButtonAction)
+Events.OnObjectAdded.Add(GamePad.OnObjectAdded)
 
 -- Hide the progress bar for walking to the ladder because having one is beyond pointless.
 
-Ladders.ISBaseTimedAction = {
+GamePad.ISBaseTimedAction = {
 	create = ISBaseTimedAction.create
 }
 
 function ISBaseTimedAction:create()
-	Ladders.ISBaseTimedAction.create(self)
-	if Ladders.enRoute then
+	GamePad.ISBaseTimedAction.create(self)
+	if GamePad.enRoute then
 		self.action:setUseProgressBar(false)
-		Ladders.enRoute = nil
+		GamePad.enRoute = nil
 	end
 end
+
+return GamePad
